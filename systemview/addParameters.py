3591a43b03dc11268b63648c57b000f658c0876f
Michael Pope <michael.pope@telstra.com>
2016-10-29 15:40:45 +1100

    Add trade list import

---
+++ systemview/addParameters.py
@@ -0,0 +1,46 @@
+"""
+Parameters for the SystemView
+trading statistics display program
+"""
+# file names
+file1 = "spx.csv"
+file2 = "simple system Trade List.csv"
+# indicator constants
+maLength = 21
+# print debug info
+verbose = True
+# start and end dates "yyyy-mm-dd"
+start = "1960-01-01"
+endDate = "1970-01-01"
+# visualizations to display
+# show a plot of price
+displayPriceGraph = True
+# show a plot of price
+displayPriceTradesGraph = True
+# distance in percent from price fro signal markers
+distance = 0.2
+# show a plot of all trades
+displayTradeGraph = True
+# show a plot ot trades versus time
+displayTradesVersusTime = True
+# show a plot of the equity curve
+displayEquityCurve = True
+# show a log plot of the equity curve
+displayEquityCurveLog = True
+# show a graph of the distribution of returns
+displayDistribution = True
+# show a graph of drawdowns
+displayDrawdownGraph = True
+# show a graph of time in drawdown
+displayTimeInDrawDown = True
+# show a graph of Maximum Adverse Excursion
+displayMAE = True
+# show a graph of in-trade volatility
+displayInTradeVol = True
+# show a graph of Efficiency
+displayEfficiency = True
+# show a graph of profit by calendar year
+displayYearlyProfit = True
+
+# all done!
+
